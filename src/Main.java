8f0cc51cea0a5d4e881f39b46afd0fe0d33a1c5e
Hydreos <cassagneivan@gmail.com>
2025-01-07 04:01:16 +0100

    réorganisation/commentaires

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        // AFN -> AFD
         Automate afn = LectureFichier.lireFichier("afn.txt");
-        System.out.println("\nAFN");
-        afn.affiche();
-
         Automate afd = new Automate();
         afd.conversionAfnAfd(afn.etats.get(1), afn);
         afd.remplirSymboles();
-        System.out.println("\nAFD :");
-        afd.affiche();
         EcritureFichier.ecrireAutomate("afd.txt", afd);
 
+        // AFD -> AFD minimisé
         Automate afd2 = LectureFichier.lireFichier("afd2.txt");
         Automate afd2mini = Automate.minimisationAfd(afd2);
         EcritureFichier.ecrireAutomate("afd2mini.txt", afd2mini);
